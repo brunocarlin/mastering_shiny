300cf0db2190f1f5793d443107ff2dc10241564d
runner <runner@Mac-1481.local>
2020-06-05 22:52:08 +0000

    Style

--- R/run_chapter.R
+++ R/run_chapter.R
@@ -7,24 +7,26 @@
 #'
 #' @value an R shiny app
 #'
-#'@examples
+#' @examples
 #'
 #' \dontrun{
-#'    run_chapter(3)
-#'    run_chapter(2.2)
+#' run_chapter(3)
+#' run_chapter(2.2)
 #' }
 run_chapter <- function(chapter_path) {
   first_path <- paste0("chapters/", chapter_path)
   second_path <-
-    paste0("chapters/",
-           stringr::str_extract(chapter_path, "[:digit:]+"),
-           "/",
-           chapter_path)
-  if (file.exists(paste0(first_path,"/app.r"))) {
+    paste0(
+      "chapters/",
+      stringr::str_extract(chapter_path, "[:digit:]+"),
+      "/",
+      chapter_path
+    )
+  if (file.exists(paste0(first_path, "/app.r"))) {
     shiny::shinyAppDir(first_path)
   } else if (dir.exists(second_path)) {
     shiny::shinyAppDir(second_path)
-  } else stop("No app")
-
-
+  } else {
+    stop("No app")
+  }
 }
